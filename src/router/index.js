ed99bd85884fd60eee361c1d73f3e11a3bb502e1
mohadese-nm <mohadese.namavar@gmail.com>
2023-02-28 20:37:06 +0330

    create product detail component and add the router

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,11 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
 const routes = [
-  
+  {
+    path: '/product',
+    name: 'productDetails',
+    component: () => import ('../components/productDetails.vue')
+  }
 ]
 
 const router = createRouter({
